852b93b8c8c07a68aefcdad94758bd824457e767
Marc Girbau <marc.girbau94@gmail.com>
2024-03-15 17:13:05 +0100

    Implement length error case in validateCreditCard function

--- src/validateCreditCard/validateCreditCard.ts
+++ src/validateCreditCard/validateCreditCard.ts
@@ -1,7 +1,12 @@
 const validateCreditCard = (creditCardNumber: string) => {
-  if (creditCardNumber.length >= 16) {
-    return { isValid: true };
+  let errors;
+
+  if (creditCardNumber.length < 16) {
+    errors = { lengthError: 'The card must have at least 16 digits' };
+    return { isValid: false, errors };
   }
+
+  return { isValid: true };
 };
 
 export default validateCreditCard;
