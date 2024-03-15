c162bb394c71953651430b2247b747e395142dae
Anna Marrodán <anna.marrodan@gmail.com>
2024-03-15 17:44:11 +0100

    Create test use case credit card Luhn algorithm

--- src/validateCreditCard/validateCreditCard.spec.ts
+++ src/validateCreditCard/validateCreditCard.spec.ts
@@ -7,7 +7,7 @@ describe('Given a validateCreditCard function', () => {
 
   describe('When it receives a credit card number with 16 digits', () => {
     test('Then it should return that the credit card is valid', () => {
-      const creditCardNumber = '1234567891234567';
+      const creditCardNumber = '1789372997456783';
 
       const expectedResult = { isValid: true };
 
@@ -16,6 +16,7 @@ describe('Given a validateCreditCard function', () => {
       );
     });
   });
+
   describe('When it receives a credit card number with less than 16 digits', () => {
     test('Then it should return that the credit card is invalid and an error message', () => {
       const creditCardNumber = '123';
@@ -27,6 +28,23 @@ describe('Given a validateCreditCard function', () => {
       };
 
       expect(validateCreditCard(creditCardNumber)).toStrictEqual(
+        expectedResult,
+      );
+    });
+  });
+
+  describe('When it receives a credit card number that is not valid according to the Luhn algorithm', () => {
+    test('Then it should return that the credit card is not valid according to the Luhn algorithm', () => {
+      const nonValidCreditCardNumber = '8910423685127395';
+      const errorMessage =
+        'The card is not valid according to the Luhn algorithm';
+
+      const expectedResult = {
+        isValid: false,
+        errors: { lengthError: errorMessage },
+      };
+
+      expect(validateCreditCard(nonValidCreditCardNumber)).toStrictEqual(
         expectedResult,
       );
     });
