2395fbaf2b3ad9d90685b4fd8d1222b4a8709e36
Anna Marrodán <anna.marrodan@gmail.com>
2024-03-15 17:44:53 +0100

    Implement luhns algorithm error case in validateCreditCard function

--- src/validateCreditCard/validateCreditCard.ts
+++ src/validateCreditCard/validateCreditCard.ts
@@ -1,8 +1,16 @@
 const validateCreditCard = (creditCardNumber: string) => {
   let errors;
+  const isLuhnValid = validateLuhnsAlgorithm(creditCardNumber);
 
   if (creditCardNumber.length < 16) {
     errors = { lengthError: 'The card must have at least 16 digits' };
+    return { isValid: false, errors };
+  }
+
+  if (!isLuhnValid) {
+    errors = {
+      lengthError: 'The card is not valid according to the Luhn algorithm',
+    };
     return { isValid: false, errors };
   }
 
@@ -10,3 +18,25 @@ const validateCreditCard = (creditCardNumber: string) => {
 };
 
 export default validateCreditCard;
+
+const validateLuhnsAlgorithm = (creditCardNumber: string): boolean => {
+  let sum = 0;
+  let isSecond = false;
+
+  for (let position = creditCardNumber.length - 1; position >= 0; position--) {
+    let digit = parseInt(creditCardNumber[position]);
+
+    if (isSecond) {
+      digit = digit * 2;
+    }
+
+    if (isSecond && digit > 9) {
+      digit = digit - 9;
+    }
+
+    sum += digit;
+    isSecond = !isSecond;
+  }
+
+  return sum % 10 === 0;
+};
