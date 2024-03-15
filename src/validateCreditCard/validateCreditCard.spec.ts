202003105182937a6d580e6c9a196f4f909d9c15
Anna Marrodán <anna.marrodan@gmail.com>
2024-03-15 16:43:57 +0100

    Test validateCrediCard is defined

---
+++ src/validateCreditCard/validateCreditCard.spec.ts
@@ -0,0 +1,7 @@
+describe('Given a validateCreditCard function', () => {
+  test('it should be defined', () => {
+    let validateCrediCard;
+
+    expect(validateCrediCard).toBeDefined();
+  });
+});
