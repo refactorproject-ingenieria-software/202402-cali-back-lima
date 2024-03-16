667004d51b1cf8da7f39a9c8d1fa2b0caead0e9e
IVSP <ivansperez93@gmail.com>
2024-03-16 10:59:42 +0100

    Add validate password function

--- src/validatePassword/validatePassword.spec.ts
+++ src/validatePassword/validatePassword.spec.ts
@@ -1,3 +1,5 @@
+import { validatePassword } from './validatePassword';
+
 describe('GIVEN a validate password function', () => {
   test('THEN it should exist', () => {
     expect(validatePassword).toBeDefined();
