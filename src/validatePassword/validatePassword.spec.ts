5df8971c341a293d1994f4acf545fd8027cc28a8
IVSP <ivansperez93@gmail.com>
2024-03-16 11:27:01 +0100

    Add type of password parameter function test

--- src/validatePassword/validatePassword.spec.ts
+++ src/validatePassword/validatePassword.spec.ts
@@ -4,4 +4,13 @@ describe('GIVEN a validate password function', () => {
   test('THEN it should exist', () => {
     expect(validatePassword).toBeDefined();
   });
+
+  describe('WHEN it recives a parameter', () => {
+    test('THEN it should be a string', () => {
+      expect(validatePassword(123 as unknown as string)).toThrow(
+        'Expected a string.',
+      );
+      expect(validatePassword('string')).toBe(typeof 'string');
+    });
+  });
 });
