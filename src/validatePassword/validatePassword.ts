a4755291f40fcb0423dee54364621e94d34ae0d7
IVSP <ivansperez93@gmail.com>
2024-03-18 20:42:50 +0100

    Renaming function to validate type of input parameter

--- src/validatePassword/validatePassword.ts
+++ src/validatePassword/validatePassword.ts
@@ -5,12 +5,12 @@ export interface ValidatePassword {
   errorMessage: string[];
 }
 
-const isValidateTypeofPassword = (password: string) => {
+const ensurePasswordIsString = (password: string) => {
   if (typeof password !== 'string') throw new Error('Expected a string.');
 };
 
 export const validatePassword = (password: string): ValidatePassword => {
-  isValidateTypeofPassword(password);
+  ensurePasswordIsString(password);
 
   return validations.reduce<ValidatePassword>(
     (accumulate, value) => {
